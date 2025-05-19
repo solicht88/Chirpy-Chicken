01397e2721fc1198c6854e33b690077802e5f939
solicht88 <sakanaasai@gmail.com>
2025-05-19 17:07:17 -0400

    added firebaseConfig

---
+++ public/firebaseConfig.mjs
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+//import dotenv from "dotenv";
+
+//dotenv.config();
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export default firebaseConfig;
